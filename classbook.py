fecef9013f40ac66431f56d522179d4a43457a61
pozivnoy-sarmat <134398190+pozivnoy-sarmat@users.noreply.github.com>
2023-09-07 09:41:36 +0300

    Update classbook.py
    По ошибке отправил начальный код. Исправил

--- classbook.py
+++ classbook.py
@@ -1,37 +1,21 @@
-class Student:
-    def __init__(self, name, surname, gender):
-        self.name = name
-        self.surname = surname
-        self.gender = gender
-        self.finished_courses = []
-        self.courses_in_progress = []
-        self.grades = {}
-
-    def add_courses(self, course_name):
-        self.finished_courses.append(course_name)
-
+# Код требует доработки (и я прошу его туда отправить): перегрузка метода __str__ пока не реализована;
+# не реализованы методы сравнения и средних оценок (задание 4).
+#
+# Была проблема со словарём оценок для лекторов: если словарь объявлять внутри класса Lecturer (без self),
+# то у лекторов будет один словарь на двоих. Можно ли было реализовать решение как-то иначе, без вынесения
+# атрибута grades в материнский класс?
+#
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+        self.grades = {}
 
 
 class Lecturer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-        self.courses_list = []
-        self.grades = {}
-
-    def get_grade(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_list and course in student.courses_in_progress:
-            if course in student.courses_in_progress:
-                lecturer.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
+    pass
 
 
 class Reviewer(Mentor):
@@ -47,21 +31,83 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
 
-cool_reviewer = Reviewer('Some', 'Buddy')
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)
+
+    def grade_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+best_student_1 = Student('Иван', 'Иванов', 'мужской')
+best_student_1.courses_in_progress += ['Python']
+best_student_1.courses_in_progress += ['C++']
+best_student_1.courses_in_progress += ['JS']
+
+cool_reviewer = Reviewer('Ещё', 'Доцент')
 cool_reviewer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['C++']
+cool_reviewer.courses_attached += ['JS']
 
-cool_lecturer = Lecturer('just', 'lecturer')
-cool_lecturer.courses_list += ['Petuhon']
+cool_lecturer_1 = Lecturer('Уже', 'Профессор')
+cool_lecturer_1.courses_attached += ['Python']
+cool_lecturer_1.courses_attached += ['C++']
+cool_lecturer_1.courses_attached += ['JS']
 
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student_1, 'Python', 10)
+cool_reviewer.rate_hw(best_student_1, 'C++', 8)
+cool_reviewer.rate_hw(best_student_1, 'JS', 7)
 
-cool_lecturer.get_grade(best_student, 'Petuhon', 10)
+best_student_1.grade_lecturer(cool_lecturer_1, 'Python', 9)
+best_student_1.grade_lecturer(cool_lecturer_1, 'C++', 5)
+best_student_1.grade_lecturer(cool_lecturer_1, 'JS', 0)
 
-# print(best_student.grades)
-print(cool_lecturer.grades)
-print(cool_lecturer.courses_list)
+best_student_2 = Student('Пётр', 'Петров', 'мужской')
+best_student_2.courses_in_progress += ['Python']
+best_student_2.courses_in_progress += ['C++']
+best_student_2.courses_in_progress += ['JS']
+
+# cool_reviewer = Reviewer('Кандидат', 'Наук')
+# cool_reviewer.courses_attached += ['Python']
+
+cool_lecturer_2 = Lecturer('Доктор', 'Наук')
+cool_lecturer_2.courses_attached += ['Python']
+cool_lecturer_2.courses_attached += ['C++']
+cool_lecturer_2.courses_attached += ['JS']
+
+cool_reviewer.rate_hw(best_student_2, 'Python', 10)
+cool_reviewer.rate_hw(best_student_2, 'C++', 7)
+cool_reviewer.rate_hw(best_student_2, 'JS', 2)
+
+best_student_2.grade_lecturer(cool_lecturer_2, 'Python', 10)
+best_student_2.grade_lecturer(cool_lecturer_2, 'C++', 4)
+best_student_2.grade_lecturer(cool_lecturer_2, 'JS', 6)
+
+# print(best_student_1.grades)
+# print(cool_lecturer_1.grades)
+# print(cool_lecturer_1.courses_attached)
+# print(best_student_2.grades)
+# print(cool_lecturer_2.grades)
+# print(cool_lecturer_2.courses_attached)
+print(
+    f'Проверяющий: {cool_reviewer.name} {cool_reviewer.surname}'
+    f'\nпроверяет курсы: {" ".join(cool_reviewer.courses_attached)}\n')
+print(f'Оценки студента: {best_student_1.name} {best_student_1.surname}\n{best_student_1.grades}')
+print(f'Оценки студента: {best_student_2.name} {best_student_2.surname}\n{best_student_2.grades}')
+print(f'Оценки лектору: {cool_lecturer_1.name} {cool_lecturer_1.surname}\n{cool_lecturer_1.grades}')
+print(f'Оценки лектору: {cool_lecturer_2.name} {cool_lecturer_2.surname}\n{cool_lecturer_2.grades}')
